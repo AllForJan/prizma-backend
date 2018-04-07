862421bbaf14a45f0e62e09ada05893e607bb09f
Mário Hunka <hunka.mario@gmail.com>
2018-04-07 09:31:05 +0200

    add example endpoint

--- src/app.py
+++ src/app.py
@@ -1,7 +1,39 @@
-from flask import Flask
+import json
+import itertools
+
+from flask import Flask, render_template, jsonify, request
+from flask_restful import Resource, Api
+import psycopg2
+import psycopg2.extras
+import settings
+
 app = Flask(__name__)
+api = Api(app)
+conn = psycopg2.connect(settings.DATABASE_URL, cursor_factory=psycopg2.extras.RealDictCursor)
 
 
 @app.route('/')
-def hello_world():
-    return 'Hello, World!'
+def index():
+    return 'hello'
+
+
+class ListPO(Resource):
+
+    def get(self):
+        q = request.args.get('q', 'Brezolupy')
+
+        cur = conn.cursor()
+
+        cur.execute("select * from apa_prijimatelia where obec='{q}'".format(q=q))
+        rows = [row for row in cur]
+        cur.close()
+
+        return jsonify(
+            po=rows
+        )
+
+
+api.add_resource(ListPO, '/po/list')
+
+if __name__ == '__main__':
+    app.run()
