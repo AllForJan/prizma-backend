fe866e74fa01e900cc7eab624bb5716a4bae056d
Mário Hunka <hunka.mario@gmail.com>
2018-04-08 12:01:39 +0200

    added license

--- src/sql/select_ziadosti.sql
+++ src/sql/select_ziadosti.sql
@@ -1,3 +1,4 @@
 select a.meno, a.vymera, a.lokalita, a.rok, a.ico, a.custom_id, a.diel
 from apa_ziadosti_diely as a
 where a.custom_id={custom_id}
+order by a.rok
--- src/sql/stats_diely.sql
+++ src/sql/stats_diely.sql
@@ -2,3 +2,4 @@ select count(*) as pocet, d.rok
 from apa_ziadosti_diely d
 where d.custom_id = {custom_id}
 group by d.rok
+order by d.rok
